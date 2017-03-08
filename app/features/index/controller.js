e6a615a170d3ba44559c85e06ddcc14915320fe3
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-09 09:02:11 +1300

    Removing useless function

--- app/features/index/controller.js
+++ app/features/index/controller.js
@@ -10,10 +10,5 @@ export default Ember.Controller.extend({
     this._super.apply(this, arguments);
     this.set('socket', this.get('socketIo').socketFor('http://localhost:8080'));
     this.get('socket').emit('afterConnection', this.get('session.data.authenticated.token'));
-
-  },
-  willDestroy: function () {
-    this.get('socket').emit('beforeDisconnect', this.get('session.data.authenticated.token'));
-    this.get('socketIo').closeSocketFor('http://localhost:8080');
   }
 });
