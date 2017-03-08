c182b68e3805dcf8f95052d53a514d0943f22353
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-08 20:33:01 +1300

    Adding afterConnection socket event

--- app/features/index/controller.js
+++ app/features/index/controller.js
@@ -4,16 +4,12 @@ const { inject: { service } } = Ember;
 
 export default Ember.Controller.extend({
   socketIo: service('socket-io'),
+  session: service(),
   init: function () {
     this._super.apply(this, arguments);
     const socket = this.get('socketIo').socketFor('http://localhost:8080');
+    socket.emit('afterConnection', this.get('session.data.authenticated'));
 
-    socket.emit('test', 'yeaaaaah');
-    socket.on('connect', () => {
-      socket.on('respond', (data) => {
-        console.log(data);
-      })
-    });
   },
   willDestroy: function () {
     this.get('socketIo').closeSocketFor('http://localhost:8080');
