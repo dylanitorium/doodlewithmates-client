ffba7044b266ab99fcb0e2d1ac411ad359c85ae4
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-09 21:35:22 +1300

    Reading database changes on client via socket

--- app/features/index/controller.js
+++ app/features/index/controller.js
@@ -10,5 +10,8 @@ export default Ember.Controller.extend({
     this._super.apply(this, arguments);
     this.set('socket', this.get('socketIo').socketFor('http://localhost:8080'));
     this.get('socket').emit('afterConnection', this.get('session.data.authenticated.token'));
+    this.get('socket').on('update', (data) => {
+      console.log(data);
+    });
   }
 });
