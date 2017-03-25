912d264c5a88244bccb34bbd16d631f870557457
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-25 21:24:14 +1300

    Updating router

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || '8888';
 app.use(express.static('dist'));
+app.get('*', (req, res) => {
+  res.sendFile('./dist/index/html');
+});
 app.listen(port, '127.0.0.1', () => {
   console.log(`Listening on: http://127.0.0.1:${port}`);
 });
