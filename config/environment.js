5c3fbec6ef4f8867961603ad647a78475383eed8
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-25 23:20:47 +1300

    Updating production configuration

--- config/environment.js
+++ config/environment.js
@@ -41,6 +41,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.APP.SERVER = 'http://localhost:8080';
+    ENV.APP.FACEBOOK_APP_ID = '742896385870748';
     ENV.APP.FACEBOOK_REDIRECT = 'https://doodlewithmates.xyz';
   }
 
