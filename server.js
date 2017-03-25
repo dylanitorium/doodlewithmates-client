2fcf5ef67621e204d427f7cf2901896933724987
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-25 21:27:37 +1300

    Updating path name again

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || '8888';
 app.use(express.static('dist'));
 app.get('*', (req, res) => {
-  res.sendFile('./dist/index/html');
+  res.sendFile(path.resolve(__dirname, '.', 'dist', 'index.html');
 });
 app.listen(port, '127.0.0.1', () => {
   console.log(`Listening on: http://127.0.0.1:${port}`);
--- start.sh
+++
@@ -1,1 +0,0 @@
-pm2 start server.js --name doodlewithmates-client
