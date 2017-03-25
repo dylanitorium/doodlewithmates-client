a46a1242b3cb45287e4b19885fc722664ea90e3c
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-03-25 21:31:50 +1300

    Oops

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || '8888';
 app.use(express.static('dist'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '.', 'dist', 'index.html');
+  res.sendFile(path.resolve(__dirname, '.', 'dist', 'index.html'));
 });
 app.listen(port, '127.0.0.1', () => {
   console.log(`Listening on: http://127.0.0.1:${port}`);
